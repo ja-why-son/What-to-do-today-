72a4a9718c6ea746139b060508e7b436c53fdf7e
Jayson Chen <jayc1127@uw.edu>
2019-01-25 21:23:51 -0800

    built the scroll base, ready for further development yeeeetyeet

--- What to-do today?/AllToDoViewController.swift
+++ What to-do today?/AllToDoViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AllToDoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ What to-do today?/ToDoViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ToDoViewController.swift
+//  What to-do today?
+//
+//  Created by Jayson Chen on 2019/1/25.
+//  Copyright © 2019 Jayson Chen. All rights reserved.
+//
+
+import UIKit
+
+class ToDoViewController: UIPageViewController {
+
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newViewController(name: "AllToDoViewController"),
+                self.newViewController(name: "TodayToDoViewController")]
+    }()
+    
+    private func newViewController(name: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil) .
+            instantiateViewController(withIdentifier: name)
+    }
+    
+    
+    /*
+    private func newColoredViewController(color: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil) .
+            instantiateViewControllerWithIdentifier("\(color)ViewController")
+    }
+    */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        dataSource = self
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
+    }
+    
+}
+
+extension ToDoViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+    
+}
