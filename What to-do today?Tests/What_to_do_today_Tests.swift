9ffcc733d2d9759fbaa93689848a4f649e7afa6e
Jayson Chen <jayc1127@uw.edu>
2019-01-25 20:13:53 -0800

    Initial Commit

---
+++ What to-do today?Tests/What_to_do_today_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  What_to_do_today_Tests.swift
+//  What to-do today?Tests
+//
+//  Created by Jayson Chen on 2019/1/25.
+//  Copyright © 2019 Jayson Chen. All rights reserved.
+//
+
+import XCTest
+@testable import What_to_do_today_
+
+class What_to_do_today_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
