daa88c68278954c886522ee3952f2da8d789eb5c
Jayson Chen <jayc1127@uw.edu>
2019-02-03 14:23:14 -0800

    add table view and table cell to today box

---
+++ What to-do today?/BigBoxViewController.swift
@@ -0,0 +1,33 @@
+//
+//  BigBoxViewController.swift
+//  What to-do today?
+//
+//  Created by Jayson Chen on 2019/2/3.
+//  Copyright © 2019 Jayson Chen. All rights reserved.
+//
+
+import UIKit
+
+class BigBoxViewController: UIViewController {
+
+    @IBOutlet weak var todayBox: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        todayBox.layer.cornerRadius = 10
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
