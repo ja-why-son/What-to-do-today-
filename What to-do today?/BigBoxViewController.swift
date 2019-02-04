ae658059ef88e43525e30e7d865abeafbbd9f787
Jayson Chen <jayc1127@uw.edu>
2019-02-04 13:15:16 -0800

    bix box basic custom cell done

--- What to-do today?/BigBoxViewController.swift
+++ What to-do today?/BigBoxViewController.swift
@@ -8,26 +8,33 @@
 
 import UIKit
 
-class BigBoxViewController: UIViewController {
+class BigBoxViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var todayBox: UIView!
+    var list = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         todayBox.layer.cornerRadius = 10
-
-        // Do any additional setup after loading the view.
+        list.append("做project")
+        list.append("看雪")
+        list.append("530豆腐家吃火鍋")
+        list.append("900core meeting")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Tuple", for: indexPath) as! TodayTupleTableViewCell
+        cell.textView.text = list[indexPath.row]
+        return cell
+    }
 
+    
 }
---
+++ What to-do today?/TodayTupleTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TodayTupleTableViewCell.swift
+//  What to-do today?
+//
+//  Created by Jayson Chen on 2019/2/3.
+//  Copyright © 2019 Jayson Chen. All rights reserved.
+//
+
+import UIKit
+
+class TodayTupleTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var textView: UITextView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
