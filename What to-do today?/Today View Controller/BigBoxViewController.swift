ea2393119acf493c4408c219c99c7e46bcf07639
Jayson Chen <jayc1127@uw.edu>
2019-02-11 18:04:23 -0800

    make the table view cell expandable

--- What to-do today?/Today View Controller/BigBoxViewController.swift
+++ What to-do today?/Today View Controller/BigBoxViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class BigBoxViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var todayBox: UIView!
     var list = [String]()
+    var expandingCellHeight: CGFloat = 200
+    var expandingIndexRow: Int = 0
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,9 @@ class BigBoxViewController: UIViewController, UITableViewDataSource, UITableView
         list.append("看雪")
         list.append("530豆腐家吃火鍋")
         list.append("900core meeting")
+        expandingIndexRow = list.count - 1
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 100
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -32,9 +39,42 @@ class BigBoxViewController: UIViewController, UITableViewDataSource, UITableView
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Tuple", for: indexPath) as! TodayTupleTableViewCell
+        cell.delegate = self
         cell.textView.text = list[indexPath.row]
         return cell
     }
-
+        
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        print("height function is called!")
+        if indexPath.row == expandingIndexRow {
+            return expandingCellHeight
+        } else {
+            return 100
+        }
+    }
     
+}
+
+
+extension BigBoxViewController: ExpandingCellDelegate {
+    
+    func updated(height: CGFloat) {
+        
+        print("updated")
+
+        expandingCellHeight = height
+        
+        // Disabling animations gives us our desired behaviour
+        UIView.setAnimationsEnabled(false)
+        /* These will causes table cell heights to be recaluclated,
+         without reloading the entire cell */
+        tableView.beginUpdates()
+        tableView.endUpdates()
+        // Re-enable animations
+        UIView.setAnimationsEnabled(true)
+        
+        let indexPath = IndexPath(row: expandingIndexRow, section: 0)
+        
+        tableView.scrollToRow(at: indexPath, at: .bottom, animated: false)
+    }
 }
--- What to-do today?/Today View Controller/TodayTupleTableViewCell.swift
+++ What to-do today?/Today View Controller/TodayTupleTableViewCell.swift
@@ -8,22 +8,55 @@
 
 import UIKit
 
+protocol ExpandingCellDelegate {
+    func updated(height: CGFloat)
+}
+
+extension UITextView {
+    /**
+     Calculates if new textview height (based on content) is larger than a base height
+     
+     - parameter baseHeight: The base or minimum height
+     
+     - returns: The new height
+     */
+    func newHeight(withBaseHeight baseHeight: CGFloat) -> CGFloat {
+        
+        // Calculate the required size of the textview
+        let fixedWidth = frame.size.width
+        let newSize = sizeThatFits(CGSize(width: fixedWidth, height: .greatestFiniteMagnitude))
+        var newFrame = frame
+        
+        // Height is always >= the base height, so calculate the possible new height
+        let height: CGFloat = newSize.height > baseHeight ? newSize.height : baseHeight
+        newFrame.size = CGSize(width: max(newSize.width, fixedWidth), height: height)
+        
+        return newFrame.height
+    }
+}
+
 class TodayTupleTableViewCell: UITableViewCell {
 
     
     @IBOutlet weak var textView: UITextView!
-    
+    var delegate: ExpandingCellDelegate?
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        textView.delegate = self
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
-    
 
+}
+
+extension TodayTupleTableViewCell: UITextViewDelegate {
+    
+    func textViewDidChange(_ textView: UITextView) {
+        let height = textView.newHeight(withBaseHeight: 200)
+        delegate?.updated(height: height)
+    }
 }
