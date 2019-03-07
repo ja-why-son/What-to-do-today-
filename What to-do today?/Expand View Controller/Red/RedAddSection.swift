4136b10d0b827cf0c9ed7fe464c40e3c19fe2414
Jayson Chen <jayc1127@uw.edu>
2019-03-07 11:18:47 -0800

    red expanded view datasource and delegate connected, front end to be fixed

---
+++ What to-do today?/Expand View Controller/Red/RedAddSection.swift
@@ -0,0 +1,21 @@
+//
+//  RedAddSection.swift
+//  What to-do today?
+//
+//  Created by Jayson Chen on 2019/3/7.
+//  Copyright © 2019 Jayson Chen. All rights reserved.
+//
+
+import UIKit
+
+class RedAddTableViewCell: addTableViewCell {
+    
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        textView.delegate = self
+    }
+    
+}
---
+++ What to-do today?/Expand View Controller/Red/RedTupleViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  RedTupleViewCell.swift
+//  What to-do today?
+//
+//  Created by Jayson Chen on 2019/3/6.
+//  Copyright © 2019 Jayson Chen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RedTupleTableViewCell: ExpandableTableViewCell {
+    
+    @IBOutlet weak var checkBox: UIButton!
+    @IBOutlet weak var textView: UITextView!
+ 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        textView.delegate = self
+        checkBox.setImage(UIImage(named: "empty_checkbox"), for: .normal)
+        checkBox.setImage(UIImage(named: "checked_checkbox"), for: .selected)
+    }
+    
+    @IBAction func checkCheckBox(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        textView.text = textView.text!.replacingOccurrences(of: "\n", with: " ")
+        expandCellDelegate?.updated(height: 100)
+    }
+}
