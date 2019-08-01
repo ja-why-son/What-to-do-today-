f82b5eaa123e3fdbd38465453eacc8c11c2210f5
Jayson Chen <jayc1127@uw.edu>
2019-07-31 21:24:14 -0700

    cross out done todos in small box

--- What to-do today?/SmallBoxViewController/SmallBoxViewController + Reload.swift
+++ What to-do today?/SmallBoxViewController/SmallBoxViewController + Reload.swift
@@ -30,50 +30,67 @@ extension SmallBoxViewController {
         orangeIndexList = []
         blueIndexList = []
         greenIndexList = []
-        var redText : String = ""
-        var orangeText : String = ""
-        var blueText : String = ""
-        var greenText : String = ""
+        let redText : NSMutableAttributedString = NSMutableAttributedString()
+        let orangeText : NSMutableAttributedString = NSMutableAttributedString()
+        let blueText : NSMutableAttributedString = NSMutableAttributedString()
+        let greenText : NSMutableAttributedString = NSMutableAttributedString()
+        let paragraphstyle = NSMutableParagraphStyle()
+        paragraphstyle.paragraphSpacingBefore = 5
+        paragraphstyle.paragraphSpacing = 3
+        let attributes: [NSAttributedString.Key : Any] = [
+            .font: redTextView.font!,
+            .paragraphStyle: paragraphstyle,
+        ]
         for i in stride(from: 1, to: mainList.count, by: 1) {
-//            let addOnText = mainList[i]
-//            if mainList[i].done {
-//                let attributeString : NSMutableAttributedString = NSMutableAttributedString(string: mainList[i].content!)
-//                attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 2, range: NSMakeRange(0, attributeString.length))
-//            }
-            
+            var addOnText : NSMutableAttributedString
+            addOnText = NSMutableAttributedString(string: mainList[i].content!)
+            if mainList[i].done {
+                addOnText.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 1, range: NSMakeRange(0, addOnText.length))
+            }
             switch mainList[i].category {
             case "red":
                 // do red
                 redList.append(mainList[i])
                 redIndexList.append(i)
-                redText = redText +  mainList[i].content! + "\n"
+                redText.append(addOnText)
+                redText.append(NSAttributedString(string: "\n"))
             case "orange":
                 // do orange
                 orangeList.append(mainList[i])
                 orangeIndexList.append(i)
-                orangeText = orangeText + mainList[i].content! + "\n"
+                orangeText.append(addOnText)
+                orangeText.append(NSAttributedString(string: "\n"))
             case "blue":
                 // do blue
                 blueList.append(mainList[i])
                 blueIndexList.append(i)
-                blueText = blueText + mainList[i].content! + "\n"
+                blueText.append(addOnText)
+                blueText.append(NSAttributedString(string: "\n"))
             case "green":
                 // do green
                 greenList.append(mainList[i])
                 greenIndexList.append(i)
-                greenText = greenText + mainList[i].content! + "\n"
+                greenText.append(addOnText)
+                greenText.append(NSAttributedString(string: "\n"))
             case "none":
                 print()
             default: return
             }
         }
-        redTextView.text = redText
+        redText.addAttributes(attributes, range: NSMakeRange(0, redText.length))
+        redTextView.attributedText = redText
         redTextView.contentOffset = .zero
-        orangeTextView.text = orangeText
+        
+        orangeText.addAttributes(attributes, range: NSMakeRange(0, orangeText.length))
+        orangeTextView.attributedText = orangeText
         orangeTextView.contentOffset = .zero
-        blueTextView.text = blueText
+        
+        blueText.addAttributes(attributes, range: NSMakeRange(0, blueText.length))
+        blueTextView.attributedText = blueText
         blueTextView.contentOffset = .zero
-        greenTextView.text = greenText
+        
+        greenText.addAttributes(attributes, range: NSMakeRange(0, greenText.length))
+        greenTextView.attributedText = greenText
         greenTextView.contentOffset = .zero
     }
 }
