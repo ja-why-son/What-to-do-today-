9220b05f6a7ce757d442834f67b1ebcb71848c74
Jayson Chen <jayc1127@uw.edu>
2019-08-07 17:57:33 -0700

    fix today cell didn't expand when enter edit issue

--- What to-do today?/Today View Controller/Today View Component/TodayTupleTableViewCell.swift
+++ What to-do today?/Today View Controller/Today View Component/TodayTupleTableViewCell.swift
@@ -58,6 +58,7 @@ class TodayTupleTableViewCell: ExpandableTableViewCell {
         notToday.isHidden = false
         textRightConstraint.constant = 30
         notToday.layer.zPosition = 10000
+        expandCellDelegate?.updated()
     }
     
     func textViewDidEndEditing(_ textView: UITextView) {
