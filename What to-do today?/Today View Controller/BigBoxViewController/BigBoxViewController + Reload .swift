68e898db2d6359ebc266c902176da18ed28b037c
Jayson Chen <jayc1127@uw.edu>
2019-08-09 16:51:55 -0700

    fix clear done bug

--- What to-do today?/Today View Controller/BigBoxViewController/BigBoxViewController + Reload .swift
+++ What to-do today?/Today View Controller/BigBoxViewController/BigBoxViewController + Reload .swift
@@ -35,18 +35,21 @@ extension BigBoxViewController {
     @IBAction func clearDone(_ sender: Any) {
         // make a separated deleted list and a temp list. check thru the deleted list to see
         // if it is in the todayorderslist, then remove it. (can also count done items via deleted list
+        list = (user?.todoList)!
+        todayOrdersList = (user?.todayOrdersList)!
         Constants.heavyHaptic.impactOccurred()
         NotificationCenter.default.post(name: NSNotification.Name(rawValue: "endEdit"), object: nil)
-        var count : Int = 0
-        var tempTodo: [Todo] = []
+        var keepTodos : [Todo] = []
+        var deletedTodos : [Todo] = []
         for todo in list {
             if todo.done {
-                count = count + 1
+                deletedTodos.append(todo)
             } else {
-                tempTodo.append(todo)
+                keepTodos.append(todo)
             }
         }
-        if count == 0 {
+        let doneCount = deletedTodos.count
+        if doneCount == 0 {
             let alert = UIAlertController(title: NSLocalizedString("Hmm", comment: ""), message: NSLocalizedString("You haven't done any todo yet...", comment: ""), preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: UIAlertAction.Style.default, handler: nil))
             self.present(alert, animated: true, completion: nil)
@@ -54,9 +57,18 @@ extension BigBoxViewController {
             let alert = UIAlertController(title: NSLocalizedString("Clear done", comment: ""), message: NSLocalizedString("Clear all the done todos?", comment: ""), preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: UIAlertAction.Style.default, handler: nil))
             alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: UIAlertAction.Style.default, handler: { action in
+                
+                for deletedTodo in deletedTodos {
+                    if deletedTodo.isToday {
+                        self.todayOrdersList.remove(at: self.todayOrdersList.index(of: deletedTodo.uuid)!)
+                    }
+                }
+                
                 self.user?.todoList = []
+                self.user?.todayOrdersList = []
                 PersistenceService.saveContext()
-                self.user?.todoList = tempTodo
+                self.user?.todayOrdersList = self.todayOrdersList
+                self.user?.todoList = keepTodos
                 PersistenceService.saveContext()
                 self.reloadToday()
                 
@@ -64,8 +76,8 @@ extension BigBoxViewController {
                 let head = NSLocalizedString("You finished ", comment: "")
                 let body = NSLocalizedString(" today!", comment: "")
                 let tail = NSLocalizedString("Keep it going!", comment: "")
-                let placeholder : String = count == 1 ? NSLocalizedString("todo", comment: "") : NSLocalizedString("todos", comment: "")
-                let comment = head + String(count) + placeholder + body + "\n" + tail
+                let placeholder : String = doneCount == 1 ? NSLocalizedString("todo", comment: "") : NSLocalizedString("todos", comment: "")
+                let comment = head + String(doneCount) + placeholder + body + "\n" + tail
                 let congrat = UIAlertController(title: NSLocalizedString("YAY", comment: ""), message: comment, preferredStyle: .alert)
                 congrat.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: UIAlertAction.Style.default, handler: nil))
                 self.present(congrat, animated: true, completion: nil)
